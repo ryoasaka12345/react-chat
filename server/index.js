627f7736f7767657e5084e19a843f8f4972844b4
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-08-05 11:46:32 +0900

    socket connection test

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const coketIo = require("socket.io");
+const express = require("express"); // helps manage sarver
+const cors = require("cors"); // make the backend to the frontend per CORS.
+const socketIo = require("socket.io");
 
+// build server
 var server = http.createServer();
 
+// app helps manage sarver
 const app = express();
 app.use(cors());
 
-const PORT = 4000;
+//make websocket
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
 
+io.on("connection", (socket) => {
+    console.log(`New client connected`);
+});
+
+// launch server 
+const PORT = 4000;
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
