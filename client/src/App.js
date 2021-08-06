94ed59a0251cfc2813c79df74c5de7c16fa75af0
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-08-06 11:04:11 +0900

    create chat box and message form

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "localhost:4000";
 const socket = socketIOClient(ENDPOINT);
 
 function App() {
-  const [message, setMessage] = useState("Hello");
+  const [receivedMessage, setReceivedMessage] = useState([]);
+  const [messageLog, setMessageLog] = useState();
+  const [roomName, setRoomName] = useState();
+  const messageInput = useRef();
+
+
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("connect");
-  }, []);
+    setMessageLog(receivedMessage.map((message) => (
+      <li>
+        {message.name} : {message.mess}
+      </li>
+    )));
+  }, [receivedMessage]);
 
+  /* 
+    socket
+  */
   socket.on("hello", (data) => {
     console.log(data);
-    setMessage(data);
+    setRoomName(data);
   });
+
+  socket.on("messages", (messages) => {
+    console.log(messages);
+    setReceivedMessage(messages);
+  });
+
+  /* 
+    functions
+  */
+  const submitHandler = (e) => {
+    const sendItem = {
+      name: "user",
+      mess: messageInput.current.value
+    }
+    console.log("send:", sendItem);
+    socket.emit("message", sendItem);
+  }
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <h1>{roomName}</h1>
+      <div>{messageLog}</div>
+      <form onSubmit={submitHandler}>
+        <input type="text" ref={messageInput}></input>
+        <input type="submit"></input>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const express = require("express"); // helps manage sarver
 const cors = require("cors"); // make the backend to the frontend per CORS.
 const socketIo = require("socket.io");
 
+// useStates
+let messages = [
+    { name: "master", mess: "Welcome to Asaka's chat room" },
+    { name: "master", mess: "what will you say?"},
+    { name: "master", mess: "who are you?"}
+];
+
 // build server
 var server = http.createServer();
 
@@ -19,13 +26,22 @@ const io = socketIo(server, {
     },
 });
 
+// connection
 io.on("connection", socket => {
-    console.log(`New client connect`);
-    socket.on("hello", (data) => {
-        console.log(data);
-    });
     socket.emit("hello", "Wellcome to Asaka's chat room");
+    socket.emit("messages", messages);
+
+    // message log
+    socket.on("message", message => {
+        console.log(message);
+        // messages = [message];
+        // socket.emit("message", messages);
+        messages = [...messages, message];
+        socket.emit("messages", messages);
+        console.log("send message");
+    });
 });
+
 
 // launch server 
 const PORT = 4000;
