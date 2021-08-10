49295a5928fbe32fa06caa7ba46c13e43e9460bd
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-08-10 12:43:36 +0900

    connection test

--- server/index.js
+++ server/index.js
@@ -3,13 +3,37 @@ const express = require("express"); // helps manage sarver
 const cors = require("cors"); // make the backend to the frontend per CORS.
 const socketIo = require("socket.io");
 const { create } = require("domain");
+var mysql = require('mysql');
+const { CONNREFUSED } = require("dns");
+
 
 /* build server */
 var server = http.createServer();
 
+
 /* helps manage sarver */
 const app = express();
 app.use(cors());
+
+
+/* mysql */
+var con_sql = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345",
+    database: "react_chat"
+});
+
+con_sql.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+var sql = "CREATE TABLE chat_log (name VARCHAR(255), message VARCHAR(255))";
+con_sql.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log("Table created");
+});
 
 /* websocket */
 const io = socketIo(server, {
@@ -30,6 +54,7 @@ let messages = {
 io.on("connection", socket => {
     require("./socket.js")(io, socket, messages);
 });
+
 
 /* launch server  */
 const PORT = 4000;
